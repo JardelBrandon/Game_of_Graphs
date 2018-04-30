5e5f379a70ddd83675c112bbb1ad7aea5b10f9bd
Jardel Brandon de Araujo Regis <jardelbrandon@hotmail.com>
2018-04-30 14:25:47 -0300

    Chat
    Adicionando programa servidor-cliente de chat para realizar teste de aplicações com socket

---
+++ Socket/Chat/Servidor/src/Servidor.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import service.ServidorService;
+
+/**
+ *
+ * @author Jardel
+ */
+public class Servidor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new ServidorService();
+    }
+}
