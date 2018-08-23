8cbf994e3c6d31337a2079b5035fdb6999aefe61
Joao Santos <j_ssfilho@hotmail.com>
2018-08-22 21:31:15 -0300

    Adição de Mapa

---
+++ Android_Studio/Hero's_Graphs/app/src/main/java/com/example/jarde/heros_graphs/VerticeMapa.java
@@ -0,0 +1,40 @@
+package com.example.jarde.heros_graphs;
+
+import java.util.Random;
+import java.util.ArrayList;
+
+public class VerticeMapa {
+    private String nome;
+    private String cor;
+    public boolean entrada;
+    public boolean saida;
+
+    public VerticeMapa(){ }
+
+    public VerticeMapa(String nome){
+        this.nome=nome;
+        this.saida=false;
+        this.entrada=false;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+    public String getNome() {
+        return nome;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String toString(){
+        return this.nome;
+    }
+
+
+}
